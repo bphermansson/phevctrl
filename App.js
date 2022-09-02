b7629b52816c52a1c1bff556cb3e41af841a895e
Patrik Hermansson <hermansson.patrik@gmail.com>
2022-09-02 19:21:06 +0200

    First version that gets data from car

--- App.js
+++ App.js
@@ -1,5 +1,8 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View, Button, Alert} from 'react-native';
+import { StyleSheet, Text, View, Button, Alert, TouchableOpacity} from 'react-native';
+import React, { useEffect, useState } from 'react';
+
+const infoText = "..."
 
 const styles = StyleSheet.create({
   container: {
@@ -19,6 +22,10 @@ const styles = StyleSheet.create({
     color: "yellow",
     margin: 10,
   },
+  opacityButton: {
+    color: "green",
+    margin: 10,
+  }
 });
 
 const Flex = () => {
@@ -32,15 +39,63 @@ const Flex = () => {
       <View style={{ flex: 2, backgroundColor: "darkorange" }}>
         <Text style={styles.txt}>test</Text>
         
-        <Button
+        <Button 
+          color="#f194ff"
           title="Press me"
           onPress={() => Alert.alert('Simple Button pressed')}
         />
+        <Button
+          title="Press me too"
+          onPress={() => Alrt()}
+        />
+        <Text>{infoText}</Text>
+        <TouchableOpacity onPress={_onPressButton}> 
+        <Text style = {styles.opacityButton}>
+          button name
+        </Text>
+      </TouchableOpacity >
+
+      {getLockStatus()}
+
       </View> 
       <View style={{ flex: 3, backgroundColor: "green" }} />
     </View>
   );
 };
+
+const _onPressButton = () => {
+  return (
+    Alert.alert('Alrt Button pressed')
+  );
+}
+
+
+const Alrt = () => {
+  return (
+    Alert.alert('Alrt Button pressed')
+  );
+}
+
+const getLockStatus = () => {
+  const [isLoading, setLoading] = useState(true);
+  const [data, setData] = useState([]);
+  console.log(data);
+
+  useEffect(() => {
+    fetch('http://192.168.1.190:8000/?func=lockstatus')
+      .then((response) => response.json())
+      .then((json) => setData(json))
+      .catch((error) => console.error(error))
+      .finally(() => setLoading(false));
+  }, []);
+
+  return (
+    Alert.alert("ok") 
+  );
+}
+
+  
+
 
  {/* */}
 
@@ -53,7 +108,11 @@ const App = () => {
       <Text>Open up App.js to start working! on your app!</Text>
       <StatusBar style="auto" />
     */}
+
+
+
     </View>
+
   );
 }
 
